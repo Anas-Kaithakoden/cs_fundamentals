95f22c92ea9cbd9ba170a82f25ade9eaa492bc86
Anas-kaithakoden <anaskaithakoden@gmail.com>
2026-09-23 22:49:36 +0530

    Prefix Sum + Hash Map → O(n) longest subarray with target sum

--- hash_maps.py
+++ hash_maps.py
@@ -17,5 +17,28 @@ def two_sum(nums, target):
         if needed in seen:
             return [seen[needed], i]
         seen[nums[i]] = i
-print(two_sum([5, 1, 8, 6, 3], 9))
+
+def prefix_longest_subarray_sum(nums, target):
+    seen = {0: -1}
+    prefix = 0
+    longest = 0
+    
+
+    for i, num  in enumerate(nums):
+
+        prefix += num
+
+        needed = prefix - target
+        if needed in seen:
+            longest = max(longest, i-seen[needed])
+        if prefix not in seen:
+            seen[prefix] = i
+
+    return longest
+
+
+
+
+print(prefix_longest_subarray_sum(nums = [2, 3, -2, 4, 1], target = 5))
+# print(two_sum([5, 1, 8, 6, 3], 9))
 # count_frequency()
